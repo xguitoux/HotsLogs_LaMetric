19245ffda529d4c74489098182f0d37d2e86ad9f
Marsan Guillaume <gmarsan@sqli.com>
2017-11-22 17:00:22 +0100

    Debug OS

--- server.js
+++ server.js
@@ -2,12 +2,16 @@ var app = require('./app');
 var debug = require('debug')('hots-logs:server');
 var http = require('http');
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+//var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000
+//var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
 var server = http.createServer(app);
 
-server.listen(server_port, server_ip_address, function() {
-    console.log("Listening on " + server_ip_address + ", server_port " + server_port)
-    console.log("ENV VAR " + process.env.OPENSHIFT_NODEJS_IP + ", server_port " + process.env.OPENSHIFT_NODEJS_IP)
+// server.listen(server_port, server_ip_address, function() {
+//     console.log("Listening on " + server_ip_address + ", server_port " + server_port)
+//     console.log("ENV VAR " + process.env.OPENSHIFT_NODEJS_IP + ", server_port " + process.env.OPENSHIFT_NODEJS_IP)
+// });
+
+server.listen(function() {
+    console.log(process.env);
 });
