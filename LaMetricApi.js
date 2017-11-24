6a8c6b132da2e3fd1046e6679019ec9550266e1c
Marsan Guillaume <gmarsan@sqli.com>
2017-11-24 10:59:11 +0100

    Refactoring app

---
+++ LaMetricApi.js
@@ -0,0 +1,70 @@
+module.exports = {
+    /**
+     * Method: buildResponse
+     * @return json
+     * returns LaMetric readable Json
+     */
+    buildResponse: function(req, data) {
+
+        // Building object for LaMetric. We will return it
+        var responseObj = {};
+        responseObj.frames = [];
+
+        index = 0;
+
+        var leagues = req.query.leagues.split(',');
+        var spaceLessLeagues = [];
+        // Removing white spaces
+        leagues.forEach(element => {
+
+            element = element.replace(/\s+/g, '');
+            spaceLessLeagues.push(element);
+        });
+
+        //console.log(responseObj);
+
+        if (!data) {
+            //console.log("Error: ", response.statusCode);
+            return false;
+            //res.status(response.statusCode).send(response.body);
+        } else {
+
+            data.LeaderboardRankings.forEach(element => {
+
+                if (spaceLessLeagues.indexOf(element.GameMode) >= 0) {
+
+                    var frame = {
+                        'index': index,
+                        'text': element.GameMode,
+                        'icon': 'i280',
+                    };
+                    index++;
+
+                    switch (element.LeagueID) {
+                        case 0:
+                            frame.icon = "i635"; // Master icon
+                            break;
+                        case 1:
+                            frame.icon = "i5273"; // Diams icon
+                            break;
+                        case 2:
+                            frame.icon = "i5271"; // Plat icon
+                            break;
+                        case 3:
+                            frame.icon = "i5274"; // Gold icon
+                            break;
+                        case 4:
+                            frame.icon = "i5270"; // Silver icon
+                            break;
+                        case 5:
+                            frame.icon = "i5269"; // Bronze icon
+                            break;
+                    }
+                    responseObj.frames.push(frame);
+                }
+            });
+
+            return responseObj;
+        }
+    },
+}
--- server.js
+++ server.js
@@ -1,17 +1,77 @@
-var app = require('./app');
-var debug = require('debug')('hots-logs:server');
-var http = require('http');
+var express = require('express'),
+    path = require('path'),
+    logger = require('morgan'),
+    //cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
+    http = require('http'),
+    index = require('./routes/index'),
+    app = express(),
+    HotsLogApi = require('./HotsLogsApi'),
+    LaMetricApi = require('./LaMetricApi');
 
-//var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000
-//var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-var server = http.createServer(app);
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
-// server.listen(server_port, server_ip_address, function() {
-//     console.log("Listening on " + server_ip_address + ", server_port " + server_port)
-//     console.log("ENV VAR " + process.env.OPENSHIFT_NODEJS_IP + ", server_port " + process.env.OPENSHIFT_NODEJS_IP)
-// });
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-server.listen(function() {
-    console.log(process.env);
+
+app.get('/', function(req, res) {
+    console.log("A la racine de l'app");
 });
+
+app.get('/getData', function(req, res) {
+    console.log(req.query);
+    HotsLogApi.getPlayerStats(req.headers, req.query, function(err, response, data) {
+        if (err || response.statusCode != 200) {
+            if (err) {
+                console.log(err);
+                return res.status(500).send(err)
+            } else {
+                return res.status(response.statusCode).send(response.body);
+            }
+        };
+
+        var jsonResponse = LaMetricApi.buildResponse(req, data);
+
+        if (!jsonResponse) {
+            console.log("Error: ", response.statusCode);
+            res.status(response.statusCode).send(response.body);
+        } else {
+            console.log("Response: ", jsonResponse);
+            return res.status(200).json(jsonResponse);
+        }
+    })
+
+});
+
+app.use('/', index);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+app.listen(port, ip);
+console.log('Server running on http://%s:%s', ip, port);
+
+module.exports = app;
